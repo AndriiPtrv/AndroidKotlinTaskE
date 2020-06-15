fea197ef16e4e9f55bd33308a19441d1c347a44f
Andrii Petrov <bucklya@gmail.com>
2020-06-15 19:09:32 +0300

    use timer

--- app/src/main/java/com/example/androidkotlintaske/Lifestyle.kt
+++ app/src/main/java/com/example/androidkotlintaske/Lifestyle.kt
@@ -12,6 +12,8 @@ import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
+import kotlin.concurrent.scheduleAtFixedRate
 
 class Lifestyle : Service(), SensorEventListener {
 
@@ -28,10 +30,11 @@ class Lifestyle : Service(), SensorEventListener {
             this,
             sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
             SensorManager.SENSOR_DELAY_FASTEST
-        );
+        )
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val timer = Timer("schedule", true);
         return START_STICKY
     }
 
@@ -49,39 +52,36 @@ class Lifestyle : Service(), SensorEventListener {
         val ox = event.values[0]
         val oy = event.values[1]
         val oz = event.values[2]
-//        Log.d("Sensor: ", "$ox  $oy  $oz")
+
+        val timer = Timer("schedule", true)
 
         val sp = getSharedPreferences("Colors", Context.MODE_PRIVATE)
         val editor = sp.edit()
         var red: Int = sp.getInt("red", 128)
         var green: Int = sp.getInt("red", 128)
 
-        GlobalScope.launch {
-//            for (i in 0..11) { //было 11
-                delay(5000L) // было 300000L
-                if ((ox > 10) || (ox < -10) || (oy > 10) || (oy < -10) || (oz > 10) || (oz < -11)) {
-                    if (red in 1..255) {
-                        red -= 1
-                        editor.putInt("red", red)
-                    }
-                    if (green in 1..255) {
-                        green += 1
-                        editor.putInt("green", green)
-                    }
-                } else {
-                    if (red in 1..255) {
-                        red += 1
-                        editor.putInt("red", red)
-                    }
-                    if (green in 1..255) {
-                        green -= 1
-                        editor.putInt("green", green)
-                    }
+        timer.scheduleAtFixedRate(3000L, 500L) {
+            if ((ox > 10) || (ox < -10) || (oy > 10) || (oy < -10) || (oz > 10) || (oz < -11)) {
+                if (red in 1..255) {
+                    red -= 1
+                    editor.putInt("red", red)
                 }
-                Log.d("Color: ", "$red  $green")
-//                Log.d("Id i: ", "$i")
-                editor.apply()
-            cancel()
+                if (green in 1..255) {
+                    green += 1
+                    editor.putInt("green", green)
+                }
+            } else {
+                if (red in 1..255) {
+                    red += 1
+                    editor.putInt("red", red)
+                }
+                if (green in 1..255) {
+                    green -= 1
+                    editor.putInt("green", green)
+                }
+            }
+            Log.d("Color: ", "$red  $green")
+            editor.apply()
         }
     }
 }
